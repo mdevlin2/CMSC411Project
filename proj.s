4097b0a4f2498d920586e64e393c000927f67692
Mitchell Devlin <mitchell.r.devlin@gmail.com>
2017-11-25 21:18:37 -0500

    fixed syntax errors

--- proj.s
+++ proj.s
@@ -1,67 +1,53 @@
-
-
 .text
-LDR R10, =w1
-MOV R0, #2949120
-STR R0, [R10],#4
-MOV R0, #1740967
-STR R0, [R10],#4
-MOV R0, #919789
-STR R0, [R10],#4
-MOV R0, #466945
-STR R0, [R10],#4
-MOV R0, #234379
-STR R0, [R10],#4
-MOV R0, #117304
-STR R0, [R10],#4
-MOV R0, #58666
-STR R0, [R10],#4
-MOV R0, #29335
-STR R0, [R10],#4
-MOV R0, #14668
-STR R0, [R10],#4
-MOV R0, #7334
-STR R0, [R10],#4
-MOV R0, #3667
-STR R0, [R10],#4
-MOV R0, #1833
-STR R0, [R10],#4
-MOV R0, #917
-STR R0, [R10],#4
-MOV R0, #458
-STR R0, [R10],#4
-    
-MOV R0, #14             @ Loop limit
-MOV R1, #0              @ Loop counter
-MOV R2, #1              @ x
-MOV R3, #0              @ y
-MOV R4, #35             @ z
-MOV R5, #16             @ Shift constant
+MOV r0, #14             @ Loop limit
+MOV r1, #0              @ Loop counter
+MOV r2, #1              @ x
+MOV r3, #0              @ y
+MOV r4, #35             @ z
+MOV r5, #16             @ Shift constant
+LDR r10, =A1
 
-LSL R2, R2, R5
-LSL R3, R3, R5
-LSL R4, R4, R5
+MOV r2, r2, LSL r5
+MOV r3, r3, LSL r5
+MOV r4, r4, LSL r5
+B loop
 
-loop:
-    MOV R7, R2
-    MOV R11, R4
-    LSR R12, R2, R1
-    SUB R2, R2, R12
-    LSR R13, R7, R1
-    ADD R3, R3, R13
-    SUB R4, R4, [R10], #4
-    CMP R11, 0
+negative:   
+    MOV r8, r3, LSR r1
+    ADD r2, r2, r8
+    MOV r9, r7, LSR r1
+    SUB r3, r3, r9 
+    LDR r14, [r10], #4
+    SUB r4, r4, r14
+
+loop:   
+    MOV r7, r2
+    MOV r11, r4
+    MOV r12, r2, LSR r1
+    SUB r2, r2, r12
+    MOV r13, r7, LSR r1
+    ADD r3, r3, r13
+    LDR r15, [r10], #4
+    SUB r4, r4, r15
+    CMP r11, #0
     BLE negative
-    ADD R1, R1, 1
-    CMP R1, R0
-    BLE Loop
-
-negative:
-    LSR R8, R3, R1
-    ADD R2, R2, R8
-    LSR R9, R7, R1
-    SUB R3, R3, R9 
-    SUB R4, R4, [R10], #4
+    ADD r1, r1, #1
+    CMP r1, r0
+    BLE loop
 
 .data 
-w1=.word
+A1: .word 2949120
+A2: .word 1740967
+A3: .word 919789
+A4: .word 466945
+A5: .word 234379
+A6: .word 117304
+A7: .word 58666
+A8: .word 29335
+A9: .word 14668
+A10: .word 7334
+A11: .word 3667
+A12: .word 1833
+A13: .word 917
+A14: .word 458
+
